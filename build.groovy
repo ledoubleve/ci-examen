2fd936c97d98994b5495437d63719669af9b9564
romoliere <romain.verdin@epsi.fr>
2019-03-21 12:49:53 +0100

    Fix groovy script

--- build.groovy
+++ build.groovy
@@ -8,7 +8,6 @@ job('version1') {
         }
     }
     steps {
-        shell('git clone https://github.com/romoliere/ci-examen.git')
         shell('docker run -v ${pwd}:/app --rm phpunit/phpunit ci-examen/tests')
     }
 }
