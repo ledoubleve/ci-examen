6a0f4619a33863462ec9b867daa06d42f410f410
romoliere <romain.verdin@epsi.fr>
2019-03-21 14:55:37 +0100

    Fix groovy script

--- build.groovy
+++ build.groovy
@@ -8,6 +8,10 @@ job('version1') {
         }
     }
     steps {
+        shell('docker-compose build')
+        shell('docker login --username rverdin --password fixme')
+        shell('docker push rverdin/ci_examen_verdin_nginx:1.0')
+        shell('docker push rverdin/ci_examen_verdin_php:1.0')
         shell('docker run -v ${pwd}:/app --rm phpunit/phpunit tests')
     }
 }
