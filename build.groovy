f2deeb75013a02976a8e6801da0150c927047367
romoliere <romain.verdin@epsi.fr>
2019-03-21 15:26:32 +0100

    Add V3 docker & groovy script

---
+++ build.groovy
@@ -0,0 +1,51 @@
+job('version1') {
+    scm {
+        git {
+            remote {
+                url('https://github.com/romoliere/ci-examen.git')
+            }
+            branch('v1')
+        }
+    }
+    steps {
+        shell('docker-compose build')
+        shell('docker login --username rverdin --password fixme')
+        shell('docker push rverdin/ci_examen_verdin_nginx:1.0')
+        shell('docker push rverdin/ci_examen_verdin_php:1.0')
+        shell('docker run -v ${pwd}:/app --rm phpunit/phpunit tests')
+    }
+}
+job('version2') {
+    scm {
+        git {
+            remote {
+                url('https://github.com/romoliere/ci-examen.git')
+            }
+            branch('v2')
+        }
+    }
+    steps {
+        shell('docker-compose build')
+        shell('docker login --username rverdin --password fixme')
+        shell('docker push rverdin/ci_examen_verdin_nginx:2.0')
+        shell('docker push rverdin/ci_examen_verdin_php:2.0')
+        shell('docker run -v ${pwd}:/app --rm phpunit/phpunit tests')
+    }
+}
+job('version2') {
+    scm {
+        git {
+            remote {
+                url('https://github.com/romoliere/ci-examen.git')
+            }
+            branch('v2')
+        }
+    }
+    steps {
+        shell('docker-compose build')
+        shell('docker login --username rverdin --password fixme')
+        shell('docker push rverdin/ci_examen_verdin_nginx:3.0')
+        shell('docker push rverdin/ci_examen_verdin_php:3.0')
+        shell('docker run -v ${pwd}:/app --rm phpunit/phpunit tests')
+    }
+}
